6161d4b4337de6fca9aba24b3cc6933e0994ea9b
Nic Wilson <nic.wilson@gmail.com>
2017-03-09 11:56:20 -0600

    Create functionality to remove color blocks on clicking and update the block count at the top of the page.

--- scripts/client.js
+++ scripts/client.js
@@ -1,3 +1,8 @@
+var redCount = 0;
+var greenCount = 0;
+var blueCount = 0;
+var yellowCount = 0;
+
 $(document).ready(function() {
   $('#redButton').on('click', function() {
     addBlock("red");
@@ -11,12 +16,41 @@ $(document).ready(function() {
   $('#yellowButton').on('click', function() {
     addBlock("yellow");
   });
+  $('body').on('click', '.block', removeBlock);
+  refreshCount();
 });
+
+function refreshCount () {
+  $('#redCount').text(redCount);
+  $('#greenCount').text(greenCount);
+  $('#blueCount').text(blueCount);
+  $('#yellowCount').text(yellowCount);
+}
 
 function addBlock(color) {
   $('#blockBox').append(createBlock(color));
+  window[color+"Count"]++;
+  refreshCount();
+
+}
+
+function removeBlock () {
+  if ($(this).hasClass("red")) {
+    redCount--;
+  }
+  if ($(this).hasClass("green")) {
+    greenCount--;
+  }
+  if ($(this).hasClass("blue")) {
+    blueCount--;
+  }
+  if ($(this).hasClass("yellow")) {
+    yellowCount--;
+  }
+  $(this).remove();
+  refreshCount();
 }
 
 function createBlock(color) {
-  return "<div class=\"" + color + " block\"></div>";
+  return "<div class=\"block " + color + "\"></div>";
 }
